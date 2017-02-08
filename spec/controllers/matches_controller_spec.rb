c980f62e4c3d93a5ed10465245e1d93c8a5ad401
Zack Youngren <zack@youngren.io>
2017-02-07 23:06:44 -0500

    LVM-263: Implement tests for Students controller
    https://cs5500-jira.ccs.neu.edu/browse/LVM-263
    
    * Set up FactoryGirl
      * What a garbage name
    * Some small common-sense refactors to StudentsController

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe MatchesController, type: :controller do
+  define 'POST #create' do
+    it 'creates a new match' do
+      @match_attrs = build(:match)
+      post :create, @match_attrs
+      expect(assigns(:match)).to be_persisted
+    end
+  end
 end
