97110bd412996fc6f298b9ae2c209bd21b672d0d
Yifan Xing <xing.yif@husky.neu.edu>
2017-02-09 18:40:36 -0500

    Updated MVC for Affiliate

--- app/controllers/affiliate_controller.rb
+++
@@ -1,4 +0,0 @@
-class AffiliateController < ApplicationController
-
-
-end
--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -1,8 +1,18 @@
 class Affiliate < ApplicationRecord
 
-    has_many :coordinators
-    has_many :tutors
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-    has_many :matches
-    has_many :students, through: :matches
+  has_many :coordinators
+  has_many :tutors
+
+  has_many :matches
+  has_many :students
+
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :address, presence: true, length: { maximum: 200}
+  validates :phone_number, presence: true, length: { maximum: 11}
+
 end
