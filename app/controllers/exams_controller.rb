d1bbbef585bedf84ca6c3dbb7746ab87a8b210b8
Yifan Xing <xing.yif@husky.neu.edu>
2017-03-02 22:52:53 -0500

    Cleaned up code style and Fixed Failed spec  in views/tutors/index

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -48,13 +48,14 @@ class ExamsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_exam
-      @exam = Exam.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def exam_params
-      params.require(:exam).permit(:score, :exam_date, :subject)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_exam
+    @exam = Exam.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def exam_params
+    params.require(:exam).permit(:score, :exam_date, :subject)
+  end
 end
