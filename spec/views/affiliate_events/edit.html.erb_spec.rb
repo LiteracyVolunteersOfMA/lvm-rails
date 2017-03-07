5a99b81b86ee6e4596aacd6c5007d75fe2c5c2d3
Zack Youngren <zack@youngren.io>
2017-03-06 22:07:40 -0500

    don't push broken builds

--- spec/views/affiliate_events/edit.html.erb_spec.rb
+++ spec/views/affiliate_events/edit.html.erb_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "affiliate_events/edit", type: :view do
+RSpec.describe 'affiliate_events/edit', type: :view do
   before(:each) do
-    @affiliate_event = assign(:affiliate_event, AffiliateEvent.create!())
+    tutor = assign(:tutor, create(:tutor))
+    @affiliate_event =
+      assign(:affiliate_event, create(:affiliate_event, tutor_id: tutor.id))
   end
 
-  it "renders the edit affiliate_event form" do
+  it 'renders the edit affiliate_event form' do
     render
 
-    assert_select "form[action=?][method=?]", affiliate_event_path(@affiliate_event), "post" do
+    assert_select 'form[action=?][method=?]',
+                  affiliate_event_path(@affiliate_event),
+                  'post' do
     end
   end
 end
--- spec/views/affiliate_events/new.html.erb_spec.rb
+++ spec/views/affiliate_events/new.html.erb_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "affiliate_events/new", type: :view do
+RSpec.describe 'affiliate_events/new', type: :view do
   before(:each) do
-    assign(:affiliate_event, AffiliateEvent.new())
+    @tutor = assign(:tutor, create(:tutor))
+    @affiliate_event =
+      assign(:affiliate_event, create(:affiliate_event, tutor_id: @tutor.id))
   end
 
-  it "renders new affiliate_event form" do
+  it 'renders new affiliate_event form' do
     render
 
-    assert_select "form[action=?][method=?]", affiliate_events_path, "post" do
+    assert_select 'form[action=?][method=?]', affiliate_events_path, 'post' do
     end
   end
 end
