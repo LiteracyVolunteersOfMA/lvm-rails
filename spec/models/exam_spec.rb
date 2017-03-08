6de5238eeedf706d5d22d1f20debedc3b307b138
Yifan Xing <xing.yif@husky.neu.edu>
2017-03-08 18:09:40 -0500

    Added validation spec in exam/model

--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Exam, type: :model do
       should belong_to(:tutor)
     end
   end
+
+  describe 'validations' do
+    it 'validates score presence' do
+      should validate_presence_of(:score)
+    end
+    it 'validates score length' do
+      should validate_length_of(:score)
+        .is_at_most(6)
+    end
+  end
 end
