e54040f6989f57f1bfa30ed2de70516ae11f75e6
Yifan Xing <xing.yif@husky.neu.edu>
2017-03-07 23:57:02 -0500

    Changed Exam field Datetime to Date in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170308185448) do
-
   create_table "affiliates", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
