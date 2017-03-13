248da98e7299b0175d4c7691065b4e10441fa8d7
Yifan Xing <xing.yif@husky.neu.edu>
2017-03-13 15:03:56 -0400

    Expanded score range

--- db/fixtures/exams.rb
+++ db/fixtures/exams.rb
@@ -7,7 +7,7 @@ tutors = [*1..20].shuffle
   Exam.seed do |e|
     e.subject = Faker::Internet.slug('English')
     e.exam_date = Faker::Date.between(10.years.ago, Date.today)
-    e.score = Faker::Base.numerify('##')
+    e.score = Random.rand(100.0).truncate(2)
     e.student = students.pop
     e.tutor = tutors.pop
   end
