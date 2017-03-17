542aed35c415862bc2a3760fa1239436775f2912
Nabeel El-Husayni <nabeel@ccs.neu.edu>
2017-03-17 02:50:05 -0400

    [WIP] LVM-426 / LVM-427: Add/edit/remove tags and tagged object filtering
    https://cs5500-jira.ccs.neu.edu/browse/LVM-426
    https://cs5500-jira.ccs.neu.edu/browse/LVM-427

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,51 @@
 class TagsController < ApplicationController
+  before_action :set_tag, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+    @tagged = Tagging.where(tag_id: @tag.id).map do |tagging|
+      if !tagging.tutor_id.nil?
+        Tutor.find(tagging.tutor_id)
+      else
+        Student.find(tagging.student_id)
+      end
+    end
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def edit; end
+
+  def update
+    if @tag.update(tag_params)
+      redirect_to @tag, notice: 'Tag was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tag.destroy
+    redirect_to tags_url, notice: 'Tag was successfully deleted.'
+  end
+
   def create
-    @tag = Tag.create(create_tag_params)
+    @tag = Tag.create(tag_params)
     render json: @tag
   end
 
   private
 
-  def create_tag_params
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
     params.require(:tag).permit(:name)
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,8 @@ class Tag < ApplicationRecord
   has_many :taggings
   has_many :students, through: :taggings
   has_many :tutors, through: :taggings
+
+  def count
+    Tagging.where(tag_id: id).count
+  end
 end
