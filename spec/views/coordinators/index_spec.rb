42471335fdd14efc1047887834c756a51aa8b875
Joseph Lally <lally.j@husky.neu.edu>
2017-03-26 17:11:21 -0400

    changes after Zack's comments/suggestions

--- spec/views/coordinators/index_spec.rb
+++ spec/views/coordinators/index_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 RSpec.describe 'coordinators/index', type: :view do
   before(:each) do
     @coordinator = create(:coordinator, phone_number: '(999) 999-9999')
-    assign(:coordinators, [@coordinator])
+    @coordinator2 = create(:coordinator, phone_number: '(999) 999-9999')
+    assign(:coordinators, [@coordinator, @coordinator2])
   end
 
   it 'renders a list of coordinators' do
     render
     expect(rendered).to match(@coordinator.name)
-    expect(rendered).to match(@coordinator.date_of_birth.strftime('%Y-%m-%d'))
-    expect(rendered).to match(/\(999\) 999-9999/)
-    expect(rendered).to match(@coordinator.email)
+    expect(rendered).to match(@coordinator2.name)
   end
 end
