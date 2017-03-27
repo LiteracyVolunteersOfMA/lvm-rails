960f6da3848a30a0b0f6b98abe405b86dab30329
Joseph Lally <lally.j@husky.neu.edu>
2017-03-27 19:29:38 -0400

    removed reference to tutor_id

--- db/fixtures/assessments.rb
+++ db/fixtures/assessments.rb
@@ -9,6 +9,6 @@ tutors = [*1..20].shuffle
     a.date = Faker::Date.between(10.years.ago, Date.today)
     a.score = Random.rand(100.0).truncate(2)
     a.student_id = students.pop
-    a.tutor_id = tutors.pop
+#    a.tutor_id = tutors.pop
   end
 end
