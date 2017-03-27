33054cf87e40fcc56e0f630ce8313890f55cfee9
Yifan Xing <xing.yif@husky.neu.edu>
2017-03-27 15:40:34 -0400

    Fixed migration error

--- db/migrate/20170327150403_refactor_exam_to_assessment_in_student.rb
+++ db/migrate/20170327150403_refactor_exam_to_assessment_in_student.rb
@@ -1,7 +1,5 @@
 class RefactorExamToAssessmentInStudent < ActiveRecord::Migration[5.0]
   def change
-    remove_index :students, :exam_id
-    add_index :students, :assessment_id
     rename_column :students, :exam_id, :assessment_id
   end
 end
