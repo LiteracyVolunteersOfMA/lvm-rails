3c4d258f908579d7cf4db4cfa99893f7085c19f1
Viviano Cantu <vivi.cantu@gmail.com>
2017-03-28 21:35:10 -0400

    add student migration and model vals

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,10 @@
 class Student < ApplicationRecord
+  VALID_EMAIL_REGEX  = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PHONE_REGEX  = /\A\([0-9]{3}\) [0-9]{3}-[0-9]{4}\z/
+  VALID_ZIP_REGEX    = /\A[0-9]{5}\z/
+  VALID_SMARTT_REGEX = /\A[0-9]{4}-[0-9]{6}\z/
+  LAST_NAME_ID_REGEX = /\A[0-9]{4,5}\z/
+  
   has_many :matches
   has_many :tutors, through: :matches
 
@@ -7,15 +13,16 @@ class Student < ApplicationRecord
   has_many :assessments
 
   has_many :affiliates
+  has_many :student_comments
 
   has_many :taggings
   has_many :tags, through: :taggings
 
   validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :last_name, presence: true
   # validates :dob, presence: true
-  # validates :gender, presence: true
-  # validates :smartt_id, presence: true
+  validates :gender, presence: true
+  validates :smartt_id, presence: true
   # validates :address1, presence: true
   # validates :address2, presence: true
   # validates :city, presence: true
@@ -29,8 +36,46 @@ class Student < ApplicationRecord
   # validates :race, presence: true
   # validates :is_hispanic, presence: true
   # validates :origin_country, presence: true
+  
+  validates :cell_phone,   format: { with: VALID_PHONE_REGEX }
+  validates :home_phone,   format: { with: VALID_PHONE_REGEX }
+  validates :last_name_id, format: { with: LAST_NAME_ID_REGEX },
+                           allow_blank: true
+  validates :work_phone,  format: { with: VALID_PHONE_REGEX },
+                           allow_blank: true
+  validates :alternate_number,  format: { with: VALID_PHONE_REGEX },
+                           allow_blank: true
+  validates :emergency_number,  format: { with: VALID_PHONE_REGEX },
+                           allow_blank: true
+  validates :smartt_id,    format: { with: VALID_SMARTT_REGEX },
+                           allow_blank: true
+  validates :zip,          format: { with: VALID_ZIP_REGEX }
+  
+  validates :email, length: { maximum: 255 },
+                              format: { with: VALID_EMAIL_REGEX },
+                              uniqueness: { case_sensitive: false },
+                              allow_blank: true
 
   def name
     [first_name, last_name].join(' ')
   end
+  
+  # ======================
+  # needs prefernce arrays
+  # ======================
+  
+  def all_tags=(names)
+    self.tags = names.reject(&:empty?).uniq.map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    tags.map(&:name)
+  end
+
+  # ======================
+  # needs status class
+  # ======================
+  
 end
