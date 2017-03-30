6c62f90496bd07c47016c2064e4230a11935887c
Viviano Cantu <viviano.cantu@gmail.com>
2017-03-30 18:02:30 -0400

    merge from origin

--- db/migrate/20170328215033_update_new_student_fields.rb
+++ db/migrate/20170328215033_update_new_student_fields.rb
@@ -18,7 +18,7 @@ class UpdateNewStudentFields < ActiveRecord::Migration[5.0]
     add_column :students, :cdbg_female_head_of_household, :boolean
     add_column :students, :cdbg_household_size, :integer
     add_column :students, :cdbg_household_income, :integer
-    
+
     # preferences
     remove_column :students, :tutor_preference
     add_column :students, :age_preference, :integer
