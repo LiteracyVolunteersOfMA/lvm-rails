9596dc77818c9d62d24119e3d85d7fbee404a00d
Yifan Xing <xing.yif@husky.neu.edu>
2017-03-31 17:39:31 -0400

    Reseed to resolve conflict

--- app/models/tutoring_session.rb
+++ app/models/tutoring_session.rb
@@ -3,8 +3,7 @@ class TutoringSession < ApplicationRecord
   belongs_to :tutor
 
   validates :location,     presence: true
-  validates :start_time,   presence: true
-  validates :end_time,     presence: true
+  validates :hours,   presence: true
   # validates :student,      presence: true
   # validates :tutor,        presence: true
 end
