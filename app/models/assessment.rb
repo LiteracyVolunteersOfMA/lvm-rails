e7533882427bfa2fad31a08170ab3bdc712438b7
Yifan Xing <xing.yif@husky.neu.edu>
2017-03-31 16:01:59 -0400

    Added fields for Assessments in migration

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -2,4 +2,8 @@ class Assessment < ApplicationRecord
   belongs_to :student
 
   validates :score, presence: true, length: { maximum: 6 }
+  validates :subject, presence: true
+  validates :name, presence: true
+  validates :level, presence: true
+  validates :type, presence: true
 end
